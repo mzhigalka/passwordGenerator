8934f27c449fe19e5041f4d8240492ff8861591e
Matvey Zhigalka <mzhigalka@gmail.com>
2024-01-09 17:39:43 +0200

    added logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
 import './index.scss';
 import React from 'react';
-import IndexPhp from './index'
 
 function App() {
-  const [passwordGenerate, setPasswordGenerate] = React.useState('2UYsAhMdPR');
+  const passwordValues = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const symbols = '!@#$%^&()\_+?><:{}[]';
+  const numbers = '0123456789'
+  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  const [result, setResult] = React.useState('2UYsAhMd'); 
+  const [lengthPassword, setLengthPassword] = React.useState(passwordValues[0]); 
+  const [isSymbolsUse, setIsSymbolsUse] = React.useState(false); 
+  const [isNumbersUse, setIsNumbersUse] = React.useState(false); 
+
+  const handlePasswordGenerator = () => {
+    let currentResult = '';
+  
+    if (isSymbolsUse) {
+      chars += symbols;
+    };
+    if (isNumbersUse) {
+      chars += numbers;
+    };
+
+    for (let i = 0; i < lengthPassword; i += 1) {
+      const randomNamber = Math.floor(Math.random() * chars.length);
+      currentResult += chars.substring(randomNamber, randomNamber + 1);
+    };
+
+    setResult(currentResult);
+  };
+
+  const handleSymbolsUse = () => {
+    setIsSymbolsUse(!isSymbolsUse);
+  };
+
+  const handleNumberssUse = () => {
+    setIsNumbersUse(!isNumbersUse);
+  };
 
   return (
     <div className="App">
       <span className="subtitle">Пароль:</span>
-      <h2>{passwordGenerate}</h2>
+      <h2>{result}</h2>
       <div className="row">
         <span>Длина паролья:</span>
         <input 
           className="pwg-length" 
-          type="text" 
-          value="10" 
+          type="number" 
+          value={lengthPassword}
+          min={passwordValues[0]}
+          max={passwordValues[passwordValues.length - 1]}
+          onChange={(e) => setLengthPassword(parseInt(e.target.value))}
         />
       </div>
-      {/* <div className="row">
-        <span>Прописные буквы:</span>
-        <input class="styled-checkbox" id="checkbox1" type="checkbox" value="value1" />
-        <label for="checkbox1" />
-      </div> */}
       <div className="row">
         <span>Строчные буквы:</span>
         <input
           checked={true}
-          class="styled-checkbox"
+          className="styled-checkbox"
           id="checkbox2"
           type="checkbox"
           value="value1"
@@ -36,24 +67,28 @@ function App() {
         <div className="row">
           <span>Цифры:</span>
           <input 
-            class="styled-checkbox" 
+            className="styled-checkbox" 
             id="checkbox3" 
             type="checkbox" 
             value="value1" 
+            onChange={handleNumberssUse}
           />
           <label for="checkbox3" />
         </div>
       <div className="row">
         <span>Символы:</span>
         <input 
-          class="styled-checkbox" 
+          className="styled-checkbox" 
           id="checkbox4" 
           type="checkbox" 
           value="value1"
-        />        <label for="checkbox4" />
+          onChange={handleSymbolsUse}
+        />        
+        <label for="checkbox4" />
       </div>
 
-      <button>Сгенерировать</button>
+      <button onClick={handlePasswordGenerator}>Сгенерировать</button>
+      {/* <button className='btn-copy'>Скопировать</button> */}
     </div>
   );
 }
