c4ebbde9275f83b2303756d6a37b3491d84137ed
Matvey Zhigalka <mzhigalka@gmail.com>
2024-07-01 10:39:58 +0300

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,27 @@ function App() {
   const numbers = "0123456789";
   let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const isValidPasswordLength = (length: number) => length >= 8 && length <= 16;
+  const variants = {
+    initial: {
+      y: -25,
+      opacity: 0
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: 'easeOut'
+      }
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeIn'
+      }
+    }
+  };
 
   const [result, setResult] = React.useState<string>("2UYsAhMd");
   const [lengthPassword, setLengthPassword] = React.useState<number>(passwordValues[0]);
@@ -69,27 +90,6 @@ function App() {
     }
   }
 
-  const variants = {
-    initial: {
-      y: -25,
-      opacity: 0
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 1,
-        ease: 'easeOut'
-      }
-    },
-    exit: {
-      opacity: 0,
-      transition: {
-        duration: 0.5,
-        ease: 'easeIn'
-      }
-    }
-  };
 
   return (
     <div className="App">
